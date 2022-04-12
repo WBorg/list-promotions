7b937729a6284ef44e8d186edd1fd9756af730af
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-04-11 21:59:30 -0300

    versão atual com api fake

--- src/Pages/Promotion/Form/form.jsx
+++ src/Pages/Promotion/Form/form.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 
 const PagesPromotionForm = () => {
-
-    
+    const {id} = useParams();
     return(
         <div>
-            Form!
+            {!id
+             ? 'FORM NOVO REGISTRO'
+             : 'FORM EDITAR REGISTRO'
+
+            }
+            {id && <div> id: {id}</div>}
         </div>
     )
 }
