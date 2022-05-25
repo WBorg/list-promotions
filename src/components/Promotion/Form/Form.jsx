33e12359487a74672940d70570d82947d6016c56
Wborg <wilicious@gmail.com>
2022-05-25 15:20:20 -0300

    delecao de comentarios

--- src/components/Promotion/Form/Form.jsx
+++ src/components/Promotion/Form/Form.jsx
@@ -8,29 +8,19 @@ import UIButton from 'components/UI/Button/Button';
 const initialValue = {
   title: '',
   url: '',
-  imageUrl: '',
+  image: '',
   price: 0,
 }
 
 
 
 const PromotionForm = ( {id}) =>{
-    // 7
-    // Se o values for null
     const [values, setValues] = useState(id ? null: initialValue);
     const [acao, setAcao] = useState('Nova');
-    //2.1
-    // const [values, setValues] = useState(initialValue);
     const navigate = useNavigate();
 
-    // const { id } = useParams();
-    //5 
-    // console.log(id)
-
-    //6
     useEffect(() => {
         if (id) {
-          // axios.get(`https://apifake-jsonserver.herokuapp.com/promotions/${id}`)
           axios.get(`https://apilistpromotions.azurewebsites.net/promotions/${id}`)
             .then((response) => {
               setValues(response.data);
@@ -39,36 +29,24 @@ const PromotionForm = ( {id}) =>{
         }
       }, []);
     
-    //4
     function onSubmit(ev) {
         ev.preventDefault(); //não execute o comportamento default do form
     
         const method = id ? 'put' : 'post';
         const url = id
-          // ? `https://apifake-jsonserver.herokuapp.com/promotions/${id}`
           ? `https://apilistpromotions.azurewebsites.net/promotions/${id}`
-          // : 'https://apifake-jsonserver.herokuapp.com/promotions'
           : 'https://apilistpromotions.azurewebsites.net/promotions'
     
         axios[method](url, values)
           .then((response) => {
-            // history.push('/');
             navigate('/');
           });
 
-        // axios.post('http://localhost:5000/promotions', values)
-        //   .then((response) => {
-        //     navigate('/');
-        //   });
 
     }
     
-      //3 em mudança...
-    //   console.log(values)
       function onChange(ev) {
         const { name, value } = ev.target;
-        // console.log({name, value})
-                               //[name] = title,  
         setValues({ ...values, [name]: value });
       }
     
@@ -91,11 +69,10 @@ const PromotionForm = ( {id}) =>{
       }
     }
 
-    return( //1 //label = id
+    return( 
         <div>
         <h1>Buscapé Promoções</h1>
         <h2>{acao} Promoção</h2>
-        {/* 7.1  */}
         {!values
           ? (
             <div>Carregando...</div>
@@ -104,23 +81,23 @@ const PromotionForm = ( {id}) =>{
               <div>
                 <img className={formCss.promotionImage}  src={values.imageUrl} alt={values.title}  />
               </div>
-            <form onSubmit={onSubmit}> {/** 4 colocar o onSubmit*/ }
+            <form onSubmit={onSubmit}> 
               
               <div className={formCss.promotionFormGroup}>
                 <label htmlFor="title">Título</label>
-                 <input id="title" name="title" type="text"  onChange={onChange} value={values.title} /> {/*value={values.title} */}
+                 <input id="title" name="title" type="text"  onChange={onChange} value={values.title} /> 
               </div>
               <div className={formCss.promotionFormGroup}>
                 <label htmlFor="url">Link</label>
-                <input id="url" name="url" type="text" onChange={onChange} value={values.url}/> {/**value={values.url}  */}
+                <input id="url" name="url" type="text" onChange={onChange} value={values.url}/> 
               </div>
               <div className={formCss.promotionFormGroup}>
                 <label htmlFor="imageUrl">Imagem (URL)</label>
-                <input id="imageUrl" name="imageUrl" type="text" onChange={onChange}  value={values.imageUrl}/> {/**value={values.imageUrl} */}
+                <input id="imageUrl" name="imageUrl" type="text" onChange={onChange}  value={values.imageUrl}/> 
               </div>
               <div className={formCss.promotionFormGroup}>
                 <label htmlFor="price">Preço</label>
-                <input id="price" name="price" type="number" step="any" onChange={onChange}  value={values.price}/> {/**value={values.price} */}
+                <input id="price" name="price" type="number" step="any" onChange={onChange}  value={values.price}/> 
               </div>
                 {renderBtn(values)}
             </form>
